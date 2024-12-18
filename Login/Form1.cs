0353772a444824b51ebbd5bb79524e17fd763c04
pablosb04 <pdelossantos@itpfp.com>
2024-12-18 12:35:05 +0100

    commitfinal

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -22,5 +22,20 @@ namespace Login
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
